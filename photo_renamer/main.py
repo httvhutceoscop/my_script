07b7603363c7c0a3d65f17b6dced61afd5679fe0
Viet NT <vietnt.working@gmail.com>
2025-05-12 17:54:35 +0700

    update main file

--- photo_renamer/main.py
+++ photo_renamer/main.py
@@ -1,40 +1,78 @@
+import datetime
+import logging
 import tkinter as tk
 from tkinter import filedialog, messagebox
 import os
-from utils import get_photo_date, rename_with_date
+
+# Setup logging
+logging.basicConfig(filename="rename_log.log",
+                    level=logging.INFO, format='%(asctime)s - %(message)s')
+
+
+def get_file_creation_date(file_path):
+    try:
+        creation_date = datetime.datetime.fromtimestamp(
+            os.path.getmtime(file_path)).strftime('%Y-%m-%d')
+        return creation_date
+    except Exception as e:
+        logging.error(f"Error getting date for {file_path}: {e}")
+        return None
+
+
+def rename_with_date(file_path, folder_path):
+    ext = os.path.splitext(file_path)[1]
+    original_name = os.path.basename(file_path)
+
+    date_str = get_file_creation_date(file_path)
+    if not date_str:
+        return None
+
+    base_name = date_str
+    new_name = f"{base_name}{ext}"
+    i = 1
+    while os.path.exists(os.path.join(folder_path, new_name)):
+        new_name = f"{base_name}_{i}{ext}"
+        i += 1
+
+    new_path = os.path.join(folder_path, new_name)
+    os.rename(file_path, new_path)
+    return new_name
+
 
 def select_folder():
     folder = filedialog.askdirectory()
     if not folder:
         return
 
-    log_lines = []
     for filename in os.listdir(folder):
         path = os.path.join(folder, filename)
+
+        if filename.startswith(".") or filename.endswith(".DS_Store"):
+            continue
         if not os.path.isfile(path):
             continue
+
+        logging.info(f"Original path: {path}")
 
         try:
             new_name = rename_with_date(path, folder)
             if new_name:
-                log_lines.append(f"Renamed: {filename} → {new_name}")
+                logging.info(f"Renamed: {filename} → {new_name}")
             else:
-                log_lines.append(f"Skipped: {filename}")
+                logging.info(f"Skipped: {filename}")
         except Exception as e:
-            log_lines.append(f"Error: {filename} → {e}")
+            logging.error(f"Error: {filename} → {e}")
 
-    with open("rename_log.txt", "a") as log_file:
-        for line in log_lines:
-            log_file.write(line + "\n")
+    messagebox.showinfo(
+        "Done", "Đã xử lý xong. Kiểm tra file rename_log.txt để biết chi tiết.")
 
-    messagebox.showinfo("Done", "Đã xử lý xong. Kiểm tra file rename_log.txt để biết chi tiết.")
 
 # GUI
 root = tk.Tk()
-root.title("Photo Renamer")
+root.title("Renamer Tool")
 root.geometry("300x150")
 
-label = tk.Label(root, text="Chọn thư mục chứa ảnh để đổi tên:")
+label = tk.Label(root, text="Chọn thư mục chứa files để đổi tên:")
 label.pack(pady=10)
 
 btn = tk.Button(root, text="Chọn thư mục", command=select_folder)
